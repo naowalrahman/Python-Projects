6a3c69e4119810a9fec55b37d9bcf23b65fa30ab
naowalrahman <naowal.ar@gmail.com>
2020-07-17 23:39:10 -0400

    1st project addition

---
+++ code test file.py
@@ -0,0 +1,4 @@
+counter = 0
+while counter * counter < 60:
+    counter += 3
+    print(counter)
---
+++ edel-geo hw 7-15.py
@@ -0,0 +1,65 @@
+# 713 A
+print("713 A")
+print("1. C")
+print("2. C")
+print("3. A")
+print("3. D")
+
+# 713 B
+print()
+print("713 B")
+print("""9.
+__________________|____________________________
+        s1     point-A           s2
+
+segment 1 = 2x + 12
+segment 2 = 5x + 30
+entire segment = 98
+(2x + 12) + (5x +30) = 7x + 42
+7x + 42 = 98
+   - 42 - 42
+   _________
+     7x = 56
+      x = 8
+2(8) + 12 = 28 --> segment 1
+5(8) + 30 = 70 --> segment 2
+segment 1 = 28
+segment 2 = 70""")
+
+print()
+print("""10. No becuase the distance between point M and point N is 7 units,
+while the distance between point G and point H is only 3 units. Since the
+lengths of the lines are different, they are not congruent.""")
+
+print()
+print("11. Yes because both of the lines are of equal length.")
+
+# 713 B
+print()
+print("""6. ((-8 + 2)/2, (5 + 7)/2)
+= (-3, 6) <-- Answer""")
+
+print()
+print("""7. ((6 - 3)/2, (-1 - 13)/2)
+= (1.5, -7) <-- Answer""")
+
+print()
+print("""10. ((-7 + 13)/2, (16 + 2)/2)
+= (3, 9) <-- Answer""")
+
+print()
+print("""11. ((2 + x)/2, (1 + y)/2) = (4, 6)
+x = 6
+y = 11
+endpoint B = (6, 11)""")
+
+print()
+print("""12. ((4 + x)/2, (-8 + y)/2) = (-3, 2)
+x = -10
+y = 12
+endpoint G = (-10, 12)""")
+
+# 713 D
+print()
+print("713 D")
+print("I FORGOT HOW TO DO THIS I'M CONFUSED SORRY")
